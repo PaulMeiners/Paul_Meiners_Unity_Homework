2f9700acd0121e869cc79a4c6b891a0bca443d55
PaulMeiners <paulmeiners7@gmail.com>
2017-02-06 22:24:25 -0800

    Commit 4

---
+++ Meiners_Forest_Scene/Assets/Scripts/Shoot_Box.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot_Box : MonoBehaviour {
+
+	public GameObject projectile;
+	public float speed = 20;
+	private GameObject clone;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+		{
+			clone = Instantiate(projectile, transform.position, transform.rotation) as GameObject;
+			clone.GetComponent<Rigidbody>().AddForce(clone.transform.forward * speed);
+		}
+	}
+}
