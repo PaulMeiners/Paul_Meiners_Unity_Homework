ed3447d5e4e222e8ff312b4c9536f671ca58bd9f
PaulMeiners <paulmeiners7@gmail.com>
2017-03-03 13:54:20 -0800

    Commit 6 Scripts HW
    Week 7 scripting HW

---
+++ Meiners_Forest_Scene/Assets/colorSwitch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorSwitch : MonoBehaviour {
+
+	private Renderer ren;
+	private bool state = true;
+
+	public Material on;
+	public Material off;
+
+	// Use this for initialization
+	void Start () {
+		ren = this.GetComponent<Renderer> ();
+	}
+
+	void OnCollisionEnter(Collision col){
+		if (col.gameObject.tag == "playerSphere") {
+			print ("Contact");
+			state = !state;
+			if (state) {
+				ren.material = on;
+			} else {
+				ren.material = off;
+			}
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
