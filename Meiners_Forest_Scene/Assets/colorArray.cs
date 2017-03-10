e6fb4bcd429e6388ed5f9725816feebb45a59fd7
PaulMeiners <paulmeiners7@gmail.com>
2017-03-10 12:21:27 -0800

    Commit 7
    Week 8 Hw

---
+++ Meiners_Forest_Scene/Assets/colorArray.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorArray : MonoBehaviour {
+
+	public Material[] myMaterials;
+	int arrayPos = 0;
+
+	void updateMaterials ()
+	{
+		if (Input.GetMouseButtonDown (0)) {
+			arrayPos++;
+			arrayPos %= myMaterials.Length;
+			GetComponent<Renderer> ().material = myMaterials [arrayPos];
+		}
+
+
+	
+	}
+
+	void Update ()
+	{
+		updateMaterials ();
+	}
+}
---
+++ Meiners_Forest_Scene/Assets/loopTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loopTest : MonoBehaviour {
+
+		int time = 10;
+
+
+		void Start ()
+		{
+			for(int i = 0; i < time; i++)
+			{
+				Debug.Log("time: " + i);
+			}
+		}
+	}
