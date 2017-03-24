a2a9c7a80820520a4589f5479e50637b4abeefe8
PaulMeiners <paulmeiners7@gmail.com>
2017-03-23 21:11:57 -0700

    Commit 8
    Week 9 HW

---
+++ Meiners_Forest_Scene/Assets/damageCount.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damageCount : MonoBehaviour {
+
+	public float parameter1 = 100;
+	public float parameter2 = 10;
+	//define parameters
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		{
+			parameter1 = parameter1 - parameter2;
+			print(parameter1);
+		}
+
+		if(parameter1 < 0)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
